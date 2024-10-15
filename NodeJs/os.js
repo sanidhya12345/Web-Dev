0f9936a13add9a5afbf23c6b6a5478cb2d47ee26
sanidhya12345 <sanidhya.varshney_cs19@gla.ac.in>
2024-10-15 16:14:13 +0530

    os module

--- NodeJs/os.js
+++ NodeJs/os.js
@@ -2,3 +2,5 @@ const os=require("os");
 console.log(os.freemem());//how much memory left in our operating system.
 console.log(os.homedir());//C:\Users\Hp gives the home directory
 console.log(os.hostname());//DESKTOP-FBBS0CS gives the host name
+
+console.log(os.platform());//! returns about the operating system platform
