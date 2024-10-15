8566621e8de0b6c5773933ec746a2f90761fd5c0
sanidhya12345 <sanidhya.varshney_cs19@gla.ac.in>
2024-10-15 16:16:21 +0530

    os module

--- NodeJs/os.js
+++ NodeJs/os.js
@@ -4,3 +4,4 @@ console.log(os.homedir());//C:\Users\Hp gives the home directory
 console.log(os.hostname());//DESKTOP-FBBS0CS gives the host name
 
 console.log(os.platform());//! returns about the operating system platform
+console.log(os.arch());//! returns about operating system CPU architecture.
