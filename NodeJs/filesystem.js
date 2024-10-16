d70e62970a222b7974ec91bb2382d6011e2696a6
sanidhya12345 <sanidhya.varshney_cs19@gla.ac.in>
2024-10-16 15:22:38 +0530

    write file using writefile method

--- NodeJs/filesystem.js
+++ NodeJs/filesystem.js
@@ -4,18 +4,37 @@ var fs=require('fs');
 
 // Read files using file system module .
 
-fs.readFile("./read.txt",(err,data)=>{
-    if(err){
-        console.log(err);
-    }
-    console.log(data.toString());
-})
+// fs.readFile("./read.txt",(err,data)=>{
+//     if(err){
+//         console.log(err);
+//     }
+//     console.log(data.toString());
+// })
 
 //append data into the file
 
-fs.appendFile("./demo.txt","Sanidhya Varshney",(err)=>{
+// fs.appendFile("./demo.txt","Sanidhya Varshney",(err)=>{
+//     if(err){
+//         console.log(err);
+//     }
+//     console.log("data appended successfully");
+// })
+
+// open file using file system module.
+
+// fs.open("./demo.txt",'r',(err)=>{
+//     if(err){
+//         console.log(err);
+//     }
+//     console.log("file opened successfully in read mode");
+// })
+
+
+//write file method.
+
+fs.writeFile("newfile.txt","This file is generated using writefile method.",(err)=>{
     if(err){
         console.log(err);
     }
-    console.log("data appended successfully");
+    console.log("File write successfully")
 })
