778544988033cbb3dc993869d456f74644ebbc1d
sanidhya12345 <sanidhya.varshney_cs19@gla.ac.in>
2024-10-23 16:01:45 +0530

    express blog

---
+++ expressblog/data/blogs.js
@@ -0,0 +1,23 @@
+const blogs=[
+    {
+        title:"this is blog1",
+        content:"blog 1 has content related to blog1",
+        slug:"post-1"
+    },
+    {
+        title:"this is blog2",
+        content:"blog 2 has content related to blog2",
+        slug:"post-2"
+    },
+    {
+        title:"this is blog4",
+        content:"blog 3 has content related to blog3",
+        slug:"post-3"
+    },
+    {
+        title:"this is blog4",
+        content:"blog 4 has content related to blog4",
+        slug:"post-4"
+    },
+]
+module.exports=blogs;
--- expressblog/routers/blog.js
+++ expressblog/routers/blog.js
@@ -2,9 +2,21 @@ const express = require("express");
 const app = express();
 const path=require("path")
 const router=express.Router();
-
+const blogs=require("../data/blogs")
 router.get("/",(req,res)=>{
-    res.sendFile(path.join(__dirname,'../templates/index.html'));
+    res.sendFile(path.join(__dirname,'../templates/bloghome.html'));
 })
+router.get("/blog",(req,res)=>{
+    // blogs.forEach((element)=>{
+    //     console.log(element);
+    // })
+    res.sendFile(path.join(__dirname,'../templates/bloghome.html'));
+})
+router.get("/blogpost/:slug",(req,res)=>{
 
+    myBlog=blogs.filter((e)=>{
+        return e.slug==req.params.slug;
+    })
+    res.sendFile(path.join(__dirname,'../templates/blogpost.html'));
+})
 module.exports=router
