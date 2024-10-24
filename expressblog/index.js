30f3100f39577dcaa60fddda7d5e80f354b2511e
sanidhya12345 <sanidhya.varshney_cs19@gla.ac.in>
2024-10-24 15:04:10 +0530

    handlebars added

--- expressblog/index.js
+++ expressblog/index.js
@@ -1,12 +1,23 @@
 const express = require("express");
+var { engine } = require("express-handlebars");
 const app = express();
 const port = 3000;
 const path = require("path");
 
+// app.engine("handlebars", exphbs());
+
+app.engine(
+  ".handlebars",
+  engine({
+    extname: "handlebars",
+    defaultLayout: "main",
+    layoutsDir: "views/layouts",
+  })
+);
+app.set("view engine", "handlebars");
 
 app.use(express.static(path.join(__dirname, "static")));
-app.use("/",require(path.join(__dirname,"routers/blog.js")))
-
+app.use("/", require(path.join(__dirname, "routers/blog.js")));
 
 app.listen(port, () => {
   console.log(`Blog app listening on port ${port}`);
--- expressblog/routers/blog.js
+++ expressblog/routers/blog.js
@@ -1,22 +1,33 @@
 const express = require("express");
 const app = express();
-const path=require("path")
-const router=express.Router();
-const blogs=require("../data/blogs")
-router.get("/",(req,res)=>{
-    res.sendFile(path.join(__dirname,'../templates/bloghome.html'));
-})
-router.get("/blog",(req,res)=>{
-    // blogs.forEach((element)=>{
-    //     console.log(element);
-    // })
-    res.sendFile(path.join(__dirname,'../templates/bloghome.html'));
-})
-router.get("/blogpost/:slug",(req,res)=>{
+const path = require("path");
+const blogs = require("../data/blogs");
 
-    myBlog=blogs.filter((e)=>{
-        return e.slug==req.params.slug;
-    })
-    res.sendFile(path.join(__dirname,'../templates/blogpost.html'));
-})
-module.exports=router
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  // res.sendFile(path.join(__dirname, "../templates/index.html"));
+  res.render("home");
+});
+
+router.get("/blog", (req, res) => {
+  // blogs.forEach((e) => {
+  //   console.log(e.content);
+  // });
+  // res.sendFile(path.join(__dirname, "../templates/bloghome.html"));
+  res.render("blogHome", {
+    blogs: blogs,
+  });
+});
+
+router.get("/blogpost/:slug", (req, res) => {
+  myBlog = blogs.filter((e) => {
+    return e.slug == req.params.slug;
+  });
+  res.render("blogPage", {
+    title: myBlog[0].title,
+    content: myBlog[0].content,
+  });
+});
+
+module.exports = router;
